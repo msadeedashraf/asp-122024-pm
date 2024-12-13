3df55bd3088d22beebe8ae26f8bbbc5b2edac6e1
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-12-13 00:53:11 +0000

    .

---
+++ jobZilaRazor/jobZilaRazor/Pages/Contact-us.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace jobZilaRazor.Pages
+{
+    public class Contact_usModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ jobZilaRazor/jobZilaRazor/Pages/Joblisting.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace jobZilaRazor.Pages
+{
+    public class JoblistingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
