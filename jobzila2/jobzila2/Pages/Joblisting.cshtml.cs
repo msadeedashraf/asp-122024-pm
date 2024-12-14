12bbd416192aa023b6061bfa62325c3fb0d76143
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-12-14 00:33:37 +0000

    .

---
+++ jobzila2/jobzila2/Pages/Joblisting.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace jobzila2.Pages
+{
+    public class JoblistingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ jobzila2/jobzila2/Pages/Jobsearch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace jobzila2.Pages
+{
+    public class JobsearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
