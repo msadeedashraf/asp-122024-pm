76fb91c162dfa46b3cec8a5306495d5de804d997
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-01-09 01:11:48 +0000

    .

---
+++ Assignment.md
@@ -0,0 +1,4 @@
+### Assignment # 1
+Convert the previously created Tripzila project into a razor application.
+
+
--- HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12bbd416192aa023b6061bfa62325c3fb0d76143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb8999006801f9c23626ad9c0568632f6f1a32c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
